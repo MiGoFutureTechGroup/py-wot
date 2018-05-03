68f91ad4279a9782e8ffab08d2fd61f2127f6979
KaiserKatze <donizyo@gmail.com>
2018-05-03 19:38:58 +0800

    Update database config 'pywot.db'

--- pywot/db.py
+++ pywot/db.py
@@ -21,54 +21,43 @@ CREATE SCHEMA `history_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 import os
 
-'''
-映射实时数据库
-'''
-class RealtimeDatabaseRouter:
+class BaseDatabaseRouter:
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'realtime':
-            return 'realtime_db'
+        if model._meta.app_label in self.app_labels:
+            return self.db_name
+
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'realtime':
-            return 'realtime_db'
+        if model._meta.app_label in self.app_labels:
+            return self.db_name
+
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label == 'realtime' or \
-           obj2._meta.app_label == 'realtime':
-           return True
+        if obj1._meta.app_label in self.app_labels\
+                or obj2._meta.app_label in self.app_labels:
+            return True
+
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'realtime':
-            return db == 'realtime_db'
+        if app_label in self.app_labels:
+            return True
+
         return None
+
+'''
+映射实时数据库
+'''
+class RealtimeDatabaseRouter(BaseDatabaseRouter):
+    app_labels = ('realtime',)
+    db_name = 'realtime_db'
 
 '''
 映射历史数据库
 '''
-class HistoryDatabaseRouter:
-
-    def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'history':
-            return 'history_db'
-        return None
-
-    def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'history':
-            return 'history_db'
-        return None
-
-    def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label == 'history' or \
-           obj2._meta.app_label == 'history':
-           return True
-        return None
-
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'history':
-            return db == 'history_db'
-        return None
+class HistoryDatabaseRouter(BaseDatabaseRouter):
+    app_labels = ('history', 'master',)
+    db_name = 'history_db'
