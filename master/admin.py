d280454453a160116db3bfda2ecd5d46f99eda8d
KaiserKatze <donizyo@gmail.com>
2018-05-05 19:54:08 +0800

    Register models in admin site

--- master/admin.py
+++ master/admin.py
@@ -2,4 +2,13 @@
 
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Contact)
+admin.site.register(AbstractMaterial)
+admin.site.register(RealMaterial)
+admin.site.register(QuotationSheet)
+admin.site.register(QuotationDetail)
+admin.site.register(UserDetail)
