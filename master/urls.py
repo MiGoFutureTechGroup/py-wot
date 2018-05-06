ef69d0b7d28da78b73e44b4d145083c0ac3c91f3
KaiserKatze <donizyo@gmail.com>
2018-05-06 23:31:03 +0800

    Update 'master.urls'

--- master/urls.py
+++ master/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('users/', views.users),
     path('user/<int:userId>/', views.user),
 
+    path('join/', views.join),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('change_password/', views.change_password),
+
     path('materials/<int:is_real_material>/', views.materials),
     path('material/1/<int:real_material_id>', views.real_material),
 
