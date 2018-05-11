875e0f6a7d156cf0f791c2c80e032b0f58c3a941
KaiserKatze <donizyo@gmail.com>
2018-05-12 01:24:14 +0800

    Update `master.views`

--- master/views.py
+++ master/views.py
@@ -7,11 +7,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model, authenticate, login as authLogin, logout as authLogout
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import F, Q, OuterRef, Subquery, Value as V, Count, Min, Sum, Avg
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.db.models.expressions import RawSQL
 
-from .models import UserDetail, QuotationSheet, QuotationDetail, QuotationPrice
+from .models import UserDetail
 
 def _create_json(status=200, status_text=None, data=None):
     return {
@@ -57,17 +57,9 @@ def _assign_all_fields_in_model(model_instance, json_data):
 def _assign_all_fields_in_model_with_request(model_instance, request):
     _assign_all_fields_in_model(model_instance, _get_json_data_from_request(request))
 
-def _select_latest_rows(model_class, target_column_name, query_set=None):
-    query_set = query_set or model_class.objects
-    table_name = model_class._meta.db_table
-    raw_sql = '''
-    SELECT * FROM %s GROUP BY %s;
-    '''
-    return query_set.annotate(val=RawSQL(raw_sql, (table_name, target_column_name,)))
-
-def _get_working_company(request):
+def _get_working_company_id(request):
     detail = UserDetail.objects.filter(user=request.user).latest()
-    return detail.company
+    return detail.company_id
 
 ################################################################
 
@@ -75,6 +67,24 @@ def _get_working_company(request):
 def users(request):
     if request.method == 'GET':
         qs = get_user_model().objects.all()
+
+        '''
+        SELECT T3.username, T4.gender, T3.first_name, T3.last_name, T3.email, T4.nin, T4.company_id, T3.last_login, T3.date_joined, T4.cuid_id, T4.ctime
+        FROM `auth_user` AS T3
+        LEFT JOIN (
+            SELECT *
+            FROM `master_userdetail` AS T2
+            WHERE T2.`id` = (SELECT `id`
+                        FROM `master_userdetail` AS T1
+                        WHERE T1.`user_id` = T2.`user_id`
+                        ORDER BY `ctime` DESC
+                        LIMIT 1)
+        ) AS T4
+        ON T3.id = T4.user_id;
+        '''
+
+        # TODO
+        #UserDetail.objects.filter(user=OuterRef('pk')).order_by('-ctime').values('id')[:1]
 
         if qs is None:
             return JsonResponse(_create_json(status=404, status_text='No user found'))
@@ -157,50 +167,59 @@ def user(request, userId):
 ################################################################
 
 def join(request):
-    if request.user.is_authenticated:
-        return JsonResponse(_create_json(status=403, status_text='Already logined'))
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            return JsonResponse(_create_json(status=403, status_text='Already logined'))
 
-    _data = _get_json_data_from_request(request)
-    _username = _data['username']
-    _email = _data['email']
-    _password = _data['password']
+        _data = _get_json_data_from_request(request)
+        _username = _data['username']
+        _email = _data['email']
+        _password = _data['password']
 
-    _user = get_user_model().objects.create_user(_username, _email, _password)
-    _user.save()
+        _user = get_user_model().objects.create_user(_username, _email, _password)
+        _user.save()
 
-    return JsonResponse(_create_json(status=200, status_text='Join successfully'))
+        return JsonResponse(_create_json(status=200, status_text='Join successfully'))
+
+    return _unsupported_operation()
 
 def login(request):
-    if request.user.is_authenticated:
-        return JsonResponse(_create_json(status=403, status_text='Already logined'))
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            return JsonResponse(_create_json(status=403, status_text='Already logined'))
 
-    _data = _get_json_data_from_request(request)
-    _username = _data['username']
-    _password = _data['password']
+        _data = _get_json_data_from_request(request)
+        _username = _data['username']
+        _password = _data['password']
 
-    # 验证用户名和密码
-    user = authenticate(request, username=_username, password=_password)
+        # 验证用户名和密码
+        user = authenticate(request, username=_username, password=_password)
 
-    if user is not None and user.is_active():
-        # 将用户实例附着到会话
-        authLogin(request, user)
+        if user is not None and user.is_active():
+            # 将用户实例附着到会话
+            authLogin(request, user)
 
-        return JsonResponse(_create_json(status=200, status_text='Login successfully'))
+            return JsonResponse(_create_json(status=200, status_text='Login successfully'))
 
-    return JsonResponse(_create_json(status=401, status_text='Fail to login'))
+        return JsonResponse(_create_json(status=401, status_text='Fail to login'))
+
+    return _unsupported_operation()
 
 #@login_required
 def logout(request):
-    if not request.user.is_authenticated:
-        return JsonResponse(_create_json(status=403, status_text='Not yet logined'))
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return JsonResponse(_create_json(status=403, status_text='Not yet logined'))
 
-    authLogout(request)
+        authLogout(request)
 
-    return JsonResponse(_create_json(status=200, status_text='Logout successfully'))
+        return JsonResponse(_create_json(status=200, status_text='Logout successfully'))
+
+    return _unsupported_operation()
 
 #@login_required
 def change_password(request):
-    if request.user.is_active():
+    if request.method == 'POST' and request.user.is_active():
         request.user.set_password(new_password)
         return JsonResponse(_create_json(status=200, status_text='Password is changed'))
 
@@ -210,40 +229,40 @@ def change_password(request):
 
 from .models import AbstractMaterial, RealMaterial
 
-def _generate_real_material_data(real_material_inst):
+def _generate_real_material_data(material):
     return {
-        'id': real_material_inst.id,
-        'material': real_material_inst.material,
-        'provider': real_material_inst.provider,
-        'part_number': real_material_inst.part_number,
-        'guise': real_material_inst.guise,
-        'design': real_material_inst.design,
-        'photo': real_material_inst.photo,
-        'quantity_unit': real_material_inst.quantity_unit,
-        'mpq': real_material_inst.mpq,
-        'moq': real_material_inst.moq,
-        'pp': real_material_inst.pp,
-        'commment': real_material_inst.comment,
-        'cuid': real_material_inst.cuid,
-        'ctime': real_material_inst.ctime,
+        'id': material.id,
+        'material': material.material_id,
+        'provider': material.provider,
+        'part_number': material.part_number,
+        'guise': material.guise,
+        'design': material.design,
+        'photo': material.photo,
+        'quantity_unit': material.quantity_unit,
+        'mpq': material.mpq,
+        'moq': material.moq,
+        'pp': material.pp,
+        'commment': material.comment,
+        'cuid': material.cuid_id,
+        'ctime': material.ctime,
     }
 
-def _materials_real(request):
 #@login_required
+def materials_real(request):
     if request.method == 'GET':
         page = 0
         pagesize = 20
-        qs = _select_latest_rows(RealMaterial, 'part_number')[page:page + pagesize]
 
-        if qs is None:
-            return JsonResponse(_create_json(status=404, status_text='No material found'))
+        qs = RealMaterial.objects.filter(pk=Subquery(RealMaterial.objects.filter(material_id__exact=OuterRef('material_id')).order_by(F('ctime').desc(nulls_last=True)).values('pk')[:1]))
+        pagelen = qs.aggregate(pagelen=Count(F('pk')))['pagelen']
+        qs = qs[page : page + pagesize]
+
+        #print('qs.query:\n', qs.query)
 
         data = {
-            # 总页数
             'pages': page + 1,
-            # 每页显示的用户数
+            'pagelen': pagelen,
             'pagesize': pagesize,
-            # 用户列表
             'real_materials': [_generate_real_material_data(material) for material in qs],
         }
 
@@ -251,16 +270,41 @@ def _materials_real(request):
 
     return _unsupported_operation()
 
-def _materials_abstract(request):
-    return _unsupported_operation()
+def _generate_abstract_material_data(material):
+    return {
+        'id': material.id,
+        'material': material.material_id,
+        'name': material.name,
+        'brand': material.brand,
+        'part_number': material.part_number,
+        'gauge': material.gauge,
+        'comment': material.comment,
+        'cuid': material.cuid_id,
+        'ctime': material.ctime,
+    }
 
-def materials(request, is_real_material):
-    if is_real_material == 1:
-        return _materials_real(request)
 #@login_required
+def materials_abstract(request):
+    if request.method == 'GET':
+        page = 0
+        pagesize = 20
 
-    else:
-        return _materials_abstract(request)
+        qs = AbstractMaterial.objects.filter(pk=Subquery(AbstractMaterial.objects.filter(material_id__exact=OuterRef('material_id')).order_by(F('ctime').desc(nulls_last=True)).values('pk')[:1]))
+        pagelen = qs.aggregate(pagelen=Count(F('pk')))['pagelen']
+        qs = qs[page : page + pagesize]
+
+        #print('qs.query:\n', qs.query)
+
+        data = {
+            'pages': page + 1,
+            'pagelen': pagelen,
+            'pagesize': pagesize,
+            'abstract_material': [_generate_abstract_material_data(material) for material in qs],
+        }
+
+        return JsonResponse(_create_json(data=data))
+
+    return _unsupported_operation()
 
 #@login_required
 def real_material(request, real_material_id):
@@ -272,11 +316,18 @@ def real_material(request, real_material_id):
                 'real_material': _generate_real_material_data(material),
             }
 
-        except ObjectDoesNotExist as e1:
-            raise e1
+        except ObjectDoesNotExist:
+            return JsonResponse(_create_json(status=404,
+                    status_text='RealMaterial not found',
+                    data={ 'id': real_material_id, }))
 
-        except MultipleObjectsReturned as e2:
-            raise e2
+        except MultipleObjectsReturned:
+            return JsonResponse(_create_json(status=500,
+                    status_text='Too many materials found',
+                    data={
+                        'id': real_material_id,
+                        'real_materials': [_generate_real_material_data(entry) for entry in qs],
+                    }))
 
         else:
             return JsonResponse(_create_json(data=data))
@@ -288,37 +339,53 @@ def real_material(request, real_material_id):
 
 ################################################################
 
+from .models import QuotationSheet, QuotationDetail, QuotationPrice
+
 def _select_quotations(request, role):
     query_set = QuotationSheet.objects
-    company = _get_working_company(request)
+    company_id = _get_working_company_id(request)
 
     if role == 'demander':
-        return query_set.filter(demander=company)
+        return query_set.filter(demander_id=company_id)
     elif role == 'supplier':
-        return query_set.filter(supplier=company)
+        return query_set.filter(supplier_id=company_id)
     else:
         return None
+
+def _generate_quotation_data(quotation):
+    return {
+        'id': quotation.id,
+        'quotation_number': quotation.quotation_number,
+        'demander': quotation.demander_id,
+        'supplier': quotation.supplier_id,
+        'date_onset': quotation.date_onset,
+        'date_offset': quotation.date_offset,
+        'cuid': quotation.cuid_id,
+        'ctime': quotation.ctime,
+    }
 
 #@login_required
 def quotations(request, role):
     if request.method == 'GET':
         page = 0
         pagesize = 20
-        qs = _select_quotations(request, role)
 
-        if qs is None:
+        db = _select_quotations(request, role)
+
+        if db is None:
             qs = []
+            pagelen = 0
 
         else:
-            qs = _select_latest_rows(QuotationSheet, 'quotation_number', qs)[page:page + pagesize]
+            qs = db.filter(pk=Subquery(db.filter(quotation_number__exact=OuterRef('quotation_number')).order_by(F('ctime').desc(nulls_last=True)).values('pk')[:1]))
+            pagelen = qs.aggregate(pagelen=Count(F('pk')))['pagelen']
+            qs = qs[page : page + pagesize]
 
         data = {
-            # 总页数
-            'pages': 1,
-            # 每页显示的用户数
-            'pagesize': 20,
-            # 用户列表
-            'quotations': [quotation for quotation in qs],
+            'pages': page + 1,
+            'pagelen': pagelen,
+            'pagesize': pagesize,
+            'quotations': [_generate_quotation_data(quotation) for quotation in qs],
         }
 
         return JsonResponse(_create_json(data=data))
