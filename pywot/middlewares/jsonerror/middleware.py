b0b43d1907c4c655d531d265673da8805fbe1f7b
KaiserKatze <donizyo@gmail.com>
2018-05-13 22:57:52 +0800

    Update middleware `pywot.middlewares.jsonerror`

--- pywot/middlewares/jsonerror/middleware.py
+++ pywot/middlewares/jsonerror/middleware.py
@@ -4,6 +4,60 @@ from http import HTTPStatus
 
 from django.http import JsonResponse
 
+def _log(request, response):
+    print('''
+        General
+            Request URL:        {}
+            Request Method:     {}
+            Status Code:        {}
+            Remote Address:     {}
+        ------------------------------------
+        Request Headers
+            Accept:             {}
+            Accept-Encoding:    {}
+            Accept-Language:    {}
+            Connection:         {}
+            Content-Length:     {}
+            Content-Type:       {}
+            Cookie:             {}
+            Host:               {}
+            Origin:             {}
+            Referer:            {}
+            User-Agent:         {}
+        '''.format(
+            # Request URL
+            request.build_absolute_uri(),
+            # Request Method
+            request.method,
+            # Status Code
+            response.status_code,
+            # Remote Address
+            request.META.get('REMOTE_ADDR', ''),
+            # ------------------------------------
+            # Accept
+            request.META.get('HTTP_ACCEPT', ''),
+            # Accept-Encoding
+            request.META.get('HTTP_ACCEPT_ENCODING', ''),
+            # Accept-Language
+            request.META.get('HTTP_ACCEPT_LANGUAGE', ''),
+            # Connection
+            request.META.get('HTTP_CONNECTION', ''),
+            # Content-Length
+            request.META.get('CONTENT_LENGTH', ''),
+            # Content-Type
+            request.META.get('CONTENT_TYPE', ''),
+            # Cookie
+            request.META.get('HTTP_COOKIE', ''),
+            # Host
+            request.META.get('HTTP_HOST', ''),
+            # Origin
+            request.META.get('HTTP_ORIGIN', ''),
+            # Referer
+            request.META.get('HTTP_REFERER', ''),
+            # User-Agent
+            request.META.get('HTTP_USER_AGENT', ''),
+        ))
+
 class JsonErrorMiddleware:
     '''
     将 Http404 等页面转化为 JsonResponse
@@ -17,6 +71,8 @@ class JsonErrorMiddleware:
         # the view (and later middleware) are called.
 
         response = self.get_response(request)
+
+        _log(request, response)
 
         # Code to be executed for each request/response after
         # the view is called.
